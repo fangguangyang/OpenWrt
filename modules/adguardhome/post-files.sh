33ce51a540a9927b0487d37be1833970b0782c3c
fangguangyang <fangguangyang@live.com>
2023-12-04 01:52:07 +0800

    update:build shell

--- modules/adguardhome/post-files.sh
+++ modules/adguardhome/post-files.sh
@@ -1,7 +1,6 @@
 latest_ver=$(curl -s -I https://github.com/AdguardTeam/AdGuardHome/releases/latest | awk -F '/' '/^location/ {print substr($NF, 1, length($NF)-1)}')
 wget https://github.com/AdguardTeam/AdGuardHome/releases/download/${latest_ver}/AdGuardHome_linux_amd64.tar.gz
 tar -xzf AdGuardHome_linux_amd64.tar.gz
-ls -lh
-ls -lh AdGuardHome/
+mkdir -p files/usr/bin/
 mv AdGuardHome/AdGuardHome files/usr/bin/AdGuardHome
 chmod a+x files/usr/bin/AdGuardHome
