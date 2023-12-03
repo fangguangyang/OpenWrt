a8dd9af2cf752547b514c9e1fe23f8808e1e9a85
oliver <brainiac@olivers-Mac-mini.local>
2023-12-03 17:17:51 +0800

    update:build shell

--- run.sh
+++ run.sh
@@ -103,6 +103,11 @@ if [ -n "$RM_FIRST" ]; then
 fi
 
 mkdir bin/targets -p
+# macOS no need to change the owner
+# change the owner of bin to 1000:1000 when running on linux
+if [[ $(uname) =~ "Linux" ]]; then
+    sudo chown -R 1000:1000 bin
+fi
 
 compose up --remove-orphans
 build_status=$?
