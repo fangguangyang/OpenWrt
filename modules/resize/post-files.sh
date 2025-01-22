7ec0b1f980f390c8f0a0534378c3ddf6cb84618f
fangguangyang <fanngguangyang@live.com>
2025-01-22 15:58:33 +0800

    update build shell

--- modules/resize/post-files.sh
+++ modules/resize/post-files.sh
@@ -1,3 +1,3 @@
 wget -U "" -O temp.sh "https://openwrt.org/_export/code/docs/guide-user/advanced/expand_root?codeblock=0"
-sed -E 's|^(cat << "EOF" [>]?>>?) /etc/|\1 file/etc/|' temp.sh > expand-root.sh
+sed -E 's|^(cat << "EOF" [>]?>>?) /etc/|\1 files/etc/|' temp.sh > expand-root.sh
 . ./expand-root.sh
