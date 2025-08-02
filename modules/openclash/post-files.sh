aa2c9583b39f1a542f35bbbcc2865c9d52e975d5
fangguangyang <fangguangyang@live.com>
2025-08-02 16:55:42 +0800

    update:meta core

--- modules/openclash/post-files.sh
+++ modules/openclash/post-files.sh
@@ -2,7 +2,7 @@ mkdir -p files/etc/openclash/config
 wget --user-agent='clash' $CLASH_CONFIG_URL -O files/etc/openclash/config/config.yaml
 # 下载核心文件
 mkdir -p files/etc/openclash/core
-wget https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-amd64.tar.gz -O dev.tar.gz
+wget https://github.com/vernesong/OpenClash/raw/core/master/meta/clash-linux-amd64.tar.gz -O dev.tar.gz
 tar -xzf dev.tar.gz
 mv clash files/etc/openclash/core/
 
